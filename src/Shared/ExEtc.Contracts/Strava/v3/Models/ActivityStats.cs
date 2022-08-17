27749ad2493fe87cd5fd67aa2efc829cde92a9fe
maotterson <otterson.2@gmail.com>
2022-08-16 22:23:59 -0400

    allow null values in activitystats data model

--- src/Shared/ExEtc.Contracts/Strava/v3/Models/ActivityStats.cs
+++ src/Shared/ExEtc.Contracts/Strava/v3/Models/ActivityStats.cs
@@ -3,15 +3,15 @@
 namespace ExEtc.Contracts.Strava.v3.Models;
 public record ActivityStats
 {
-    [JsonPropertyName("biggest_ride_distance")] public double BiggestRideDistance { get; set; }
-    [JsonPropertyName("biggest_climb_elevation_gain")] public double BiggestClimbElevationGain { get; set; }
-    [JsonPropertyName("recent_ride_totals")] public ActivityTotal RecentRideTotals { get; set; }
-    [JsonPropertyName("recent_run_totals")] public ActivityTotal RecentRunTotals { get; set; }
-    [JsonPropertyName("recent_swim_totals")] public ActivityTotal RecentSwimTotals { get; set; }
-    [JsonPropertyName("ytd_ride_totals")] public ActivityTotal YtdRideTotals { get; set; }
-    [JsonPropertyName("ytd_run_totals")] public ActivityTotal YtdRunTotals { get; set; }
-    [JsonPropertyName("ytd_swim_totals")] public ActivityTotal YtdSwimTotals { get; set; }
-    [JsonPropertyName("all_ride_totals")] public ActivityTotal AllRideTotals { get; set; }
-    [JsonPropertyName("all_run_totals")] public ActivityTotal AllRunTotals { get; set; }
-    [JsonPropertyName("all_swim_totals")] public ActivityTotal AllSwimTotals { get; set; }
+    [JsonPropertyName("biggest_ride_distance")] public double? BiggestRideDistance { get; set; }
+    [JsonPropertyName("biggest_climb_elevation_gain")] public double? BiggestClimbElevationGain { get; set; }
+    [JsonPropertyName("recent_ride_totals")] public ActivityTotal? RecentRideTotals { get; set; }
+    [JsonPropertyName("recent_run_totals")] public ActivityTotal? RecentRunTotals { get; set; }
+    [JsonPropertyName("recent_swim_totals")] public ActivityTotal? RecentSwimTotals { get; set; }
+    [JsonPropertyName("ytd_ride_totals")] public ActivityTotal? YtdRideTotals { get; set; }
+    [JsonPropertyName("ytd_run_totals")] public ActivityTotal? YtdRunTotals { get; set; }
+    [JsonPropertyName("ytd_swim_totals")] public ActivityTotal? YtdSwimTotals { get; set; }
+    [JsonPropertyName("all_ride_totals")] public ActivityTotal? AllRideTotals { get; set; }
+    [JsonPropertyName("all_run_totals")] public ActivityTotal? AllRunTotals { get; set; }
+    [JsonPropertyName("all_swim_totals")] public ActivityTotal? AllSwimTotals { get; set; }
 }
