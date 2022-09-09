c7604d8b176797b44926d0b5b1459aaa80883fdf
maotterson <otterson.2@gmail.com>
2022-09-08 22:58:18 -0400

    remove maui app + add maui blazor app

--- src/MauiApp/ExEtc.MauiApp/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace ExEtc.MauiApp;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
---
+++ src/MauiApp/ExEtc.Mobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ExEtc.Mobile;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
