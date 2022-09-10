8ebffd317c8c0f3fb7650d33bcdc2b1425506f5b
maotterson <otterson.2@gmail.com>
2022-09-09 23:19:31 -0400

    add version to webapi controllers

--- src/WebApi/ExEtc.WebApi/Controllers/v3/ActivityController.cs
+++ src/WebApi/ExEtc.WebApi/Controllers/v3/ActivityController.cs
@@ -2,7 +2,7 @@ using ExEtc.Contracts.Strava.v3.Activities.CreateActivity;
 using ExEtc.WebApi.Application.Apis;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ExEtc.WebApi.Controllers;
+namespace ExEtc.WebApi.Controllers.v3;
 [ApiController]
 [Route("[controller]")]
 public class ActivityController : ControllerBase
